57385f443e8ff8b8c91c4100eeadb3477a7efae5
Nathanaëlle <94068090+Pythacode@users.noreply.github.com>
2026-07-24 23:42:19 +0200

    Update index.php

--- signin/index.php
+++ signin/index.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $news = 1;
 
-        $stmt = $conn->prepare("DELETE * FROM `newsletter` WHERE `id` = ?");
+        $stmt = $conn->prepare("DELETE FROM `newsletter` WHERE `id` = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $stmt->close();
