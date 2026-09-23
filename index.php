e4a9c69e1ee4c595710de773749d0060f2ca9852
Nathanaëlle <contact@nathanaelle.org>
2026-09-23 10:57:09 +0200

    same

--- index.php
+++ index.php
@@ -26,9 +26,9 @@ while ($row = $result->fetch_assoc()) {
     <style>
       <?php 
         foreach ($users as $user) {
-            echo "." . preg_replace('/[^A-Za-z0-9]/', '', $user[0]) . " {border-radius: 5px;}";
-            echo "." . preg_replace('/[^A-Za-z0-9]/', '', $user[0]) . ":hover {box-shadow: 0 0 10px 1px " . $user[1] . ";}";
-            echo "." . preg_replace('/[^A-Za-z0-9]/', '', $user[0]) . "-overlay {box-shadow: 0 0 200px 10vh " . $user[1] . ";}";
+            echo "." . preg_replace('/[^A-Za-z0-9]/u', '', $user[0]) . " {border-radius: 5px;}";
+            echo "." . preg_replace('/[^A-Za-z0-9]/u', '', $user[0]) . ":hover {box-shadow: 0 0 10px 1px " . $user[1] . ";}";
+            echo "." . preg_replace('/[^A-Za-z0-9]/u', '', $user[0]) . "-overlay {box-shadow: 0 0 200px 10vh " . $user[1] . ";}";
         }
       ?>
     </style>
