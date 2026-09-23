c1e564a080b048c55f88c914b6726f9708c44e42
Nathanaëlle <contact@nathanaelle.org>
2026-09-23 11:05:06 +0200

    same2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $users = [];
 $result = $conn->query("SELECT `id`, `username`, `color` FROM `users` WHERE admin = 1");
 
 while ($row = $result->fetch_assoc()) {
-    $users[$row['id']] = [$row['username'], $row['color']];
+    $users[$row['id']] = [$row['id'], $row['color']];
 }
 ?>
 
@@ -26,9 +26,9 @@ while ($row = $result->fetch_assoc()) {
     <style>
       <?php 
         foreach ($users as $user) {
-            echo "." . preg_replace('/[^A-Za-z0-9]/u', '', $user[0]) . " {border-radius: 5px;}";
-            echo "." . preg_replace('/[^A-Za-z0-9]/u', '', $user[0]) . ":hover {box-shadow: 0 0 10px 1px " . $user[1] . ";}";
-            echo "." . preg_replace('/[^A-Za-z0-9]/u', '', $user[0]) . "-overlay {box-shadow: 0 0 200px 10vh " . $user[1] . ";}";
+            echo "." . $user[0] . " {border-radius: 5px;}";
+            echo "." . $user[0] . ":hover {box-shadow: 0 0 10px 1px " . $user[1] . ";}";
+            echo "." . $user[0] . "-overlay {box-shadow: 0 0 200px 10vh " . $user[1] . ";}";
         }
       ?>
     </style>
@@ -176,10 +176,10 @@ while ($row = $result->fetch_assoc()) {
           } else {
             foreach ($posts as $row) {
               if ($row["type"] == "img") {
-                echo "<img id=\"" . $row["id"] . "\" src=\"/res/data/pictures/" . htmlspecialchars($row["picture_name"]) . "\" class=\"picture line-" . $row["width"] . " row-" . $row["height"] . " " . $users[$row["user_id"]][0] . "\" loading=\"lazy\">";
+                echo "<img id=\"" . $row["id"] . "\" src=\"/res/data/pictures/" . htmlspecialchars($row["picture_name"]) . "\" class=\"picture line-" . $row["width"] . " row-" . $row["height"] . " " . $row["user_id"] . "\" loading=\"lazy\">";
               } else if ($row['type'] == "video") {
                 echo '<div class="video-container">';
-                echo "<video id=\"" . $row["id"] . "\" src=\"/res/data/videos/" . htmlspecialchars($row["picture_name"]) . "\" class=\"picture line-" . $row["width"] . " row-" . $row["height"] . " " . $users[$row["user_id"]][0] . "\" loading=\"lazy\" controls></video>";
+                echo "<video id=\"" . $row["id"] . "\" src=\"/res/data/videos/" . htmlspecialchars($row["picture_name"]) . "\" class=\"picture line-" . $row["width"] . " row-" . $row["height"] . " " . $row["user_id"] . "\" loading=\"lazy\" controls></video>";
                 echo '<button class="video-action" type="button" title="Ouvrir les commentaires" onclick="display_post(' . $row["id"] . ')">⎋</button></div>';
               } else {
               }
--- res/js/home.js
+++ res/js/home.js
@@ -119,7 +119,7 @@ async function display_post(id) {
     overlay.style.display = 'block';
     quitIcon.style.display = 'block';
     fenetreModal.style.display = 'flex';
-    fenetreModal.classList = data["user"]+"-overlay";
+    fenetreModal.classList = data["user_id"]+"-overlay";
 
   } catch (err) {
     console.error(err);
